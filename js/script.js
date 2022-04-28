36920feb5a62b87d7419856f05ade5899e69cf58
Raymond Lu <raylucs15742@gmail.com>
2022-04-28 10:47:18 -0700

    add calculator solving

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let input = document.querySelector('.input');
+let score = document.querySelector('.runningscore');
+let first = 0;
+let second =0;
+let command = '';
 // 1. basic arithimetic functions
 result = 0;
 // a. add
@@ -20,14 +25,40 @@ let divide = function(one,two) {
 //2 Operate Function
 
 let operate = function(operator,first,second) {
-    if (operator === '+'){
+    if (operator === " + " ){
         add(first,second);
-    } else if (operator === '-') {
+    } else if (operator === ' - ') {
         subtract(first,second);
-    } else if (operator === '*') {
+    } else if (operator === ' * ') {
         multiply(first,second);
-    } else if (operator === '/') {
+    } else if (operator === ' / ') {
         divide(first,second);
     }
-    return result;
+    input.innerHTML = result;
 }
+const numberListen = document.querySelectorAll('.number');
+numberListen.forEach(item => {item.addEventListener('click', event => {
+    score.innerHTML += item.innerHTML;
+    input.innerHTML += item.innerHTML;
+    })
+})
+const operatorListen = document.querySelectorAll('.operator');
+operatorListen.forEach(item => {item.addEventListener('click', event => {
+    score.innerHTML += item.innerHTML;
+    first = input.innerHTML;
+    input.innerHTML = '';
+    command = item.innerHTML;
+    })
+})
+const evaluateListen = document.querySelectorAll('.evaluate');
+evaluateListen.forEach(item => {item.addEventListener('click', event => {
+    second = input.innerHTML;
+    operate(command, first , second);
+    })
+})
+const clearListen = document.querySelectorAll('.clear');
+clearListen.forEach(item => {item.addEventListener('click', event => {
+    score.innerHTML ='';
+    item.innerHTML ='';
+    })
+})
